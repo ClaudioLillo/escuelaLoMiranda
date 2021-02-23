1d98931f15999f28a67b319461f49d44aaf2b163
Claudio Lillo <cilillo@uc.cl>
2021-02-23 17:13:42 -0300

    enabled cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express, {json} from 'express'
+import morgan from 'morgan'
+const cors = require('cors')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(json())
+app.use(cors())
 
 app.get('/', (req, res)=>{
     return res.status(200).json({
