459faea99c65205a4a3f8a9dd9a932413329a0b2
Claudio Lillo <cilillo@uc.cl>
2021-02-23 03:52:41 -0300

    PORT config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from './app'
 
-app.listen(3001, ()=>{
-    console.log("listening on port 3001")
+const port = process.env.PORT || 8080
+app.listen(port, ()=>{
+    console.log("listening on port 8080")
+    console.log("espero que esto funcione")
 })
