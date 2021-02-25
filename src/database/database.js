a1ef2370891c9820c328237c6beb239641b83811
Claudio Lillo <cilillo@uc.cl>
2021-02-25 01:58:14 -0300

    pg connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,21 +1,6 @@
 import Sequelize from 'sequelize'
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        pool:{
-            max: 5,
-            min: 0,
-            require: 30000,
-            idle: 10000
-        },
-        logging: false
-    }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 export default sequelize
