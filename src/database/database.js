eeb56d557f5033b80210fe55cb38d0a7e6abc10d
Claudio Lillo <cilillo@uc.cl>
2021-02-26 01:41:58 -0300

    ssl .env

--- src/database/database.js
+++ src/database/database.js
@@ -5,11 +5,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     protocol: "postgres",
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnathorized: false
-        }
+        ssl: process.env.SSL?true:false
     }
 })
+
 
 export default sequelize
