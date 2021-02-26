8091cf97d91d38fcc712b69f653c572e9a1a48fc
Claudio Lillo <cilillo@uc.cl>
2021-02-26 01:14:23 -0300

    database connection stablished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import app from './app'
+import sequelize from './database/database.js'
 
 const port = process.env.PORT || 8080
-app.listen(port, ()=>{
-    console.log("listening on port 8080")
-    console.log("espero que esto funcione")
+
+sequelize.sync({force: true}).then(()=>{
+    app.listen(port, ()=>{
+        console.log("listening on port 8080")
+        console.log("espero que esto funcione")
+    })
 })
