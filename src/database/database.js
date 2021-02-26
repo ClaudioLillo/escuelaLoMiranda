7431507866c3f1be90166e80caa2f907531e6401
Claudio Lillo <cilillo@uc.cl>
2021-02-26 01:26:08 -0300

    sequelize config

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,11 @@ import Sequelize from 'sequelize'
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres"
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: true
+    }
 })
 
 export default sequelize
