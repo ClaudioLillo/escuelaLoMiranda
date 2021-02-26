c1d7bb2b81729bf8595dc2bfb36ac01c1b21cda0
Claudio Lillo <cilillo@uc.cl>
2021-02-26 02:01:11 -0300

    ssl dev solved

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     protocol: "postgres",
     dialectOptions: {
-        ssl: process.env.SSL?true:false
+        ssl: process.env.SSL?{
+            require: true,
+            rejectUnauthorized: false
+        }:false
     }
 })
 
