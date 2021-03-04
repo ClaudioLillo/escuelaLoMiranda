680093e87451d6373b3d108fa3acc0437ebd7323
Claudio Lillo <cilillo@uc.cl>
2021-03-04 03:39:54 -0300

    admin user seed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from './app'
 import sequelize from './database/database.js'
+import User from './models/User'
 
 const port = process.env.PORT || 8080
 
@@ -10,3 +11,32 @@ sequelize.sync({force: true}).then(()=>{
     })
 })
 .catch(err=>{console.log(err)})
+
+
+const seed = async()=>{
+    const name = "Claudio"
+    const lastName="Lillo"
+    const email="cilillo@uc.cl"
+    const password ="password"
+    const role = "admin"
+    try{
+        const newUser = await User.create({
+            name,
+            lastName,
+            email,
+            password,
+            role
+        }, {
+            fields: ['name', 'lastName', 'email', 'password', 'role']
+        })
+        if(newUser){
+            console.log("admin created")
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+seed()
+
